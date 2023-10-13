0247f717a21ea71a95085d13ca38a18739d9aaf4
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-10-13 14:57:58 -0400

    Update gen_aug.py
    Add comments to gen_aug.py

--- gen_aug.py
+++ gen_aug.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from time import sleep
 
-"""Get arguments"""
+# Get the arguments
 parser = argparse.ArgumentParser(description='Down sample SST-2 data')
 parser.add_argument('--data-path', '-d', required=True, type=str,
     help='Directory path for the data.')
@@ -18,7 +18,7 @@ api_key = args.api_key
 # Set OpenAI key:
 openai.api_key = api_key
 
-# Define genereate augmented data function:
+# Define generate augmented data function:
 def gen_aug(text):
     
     # The first part of prompt:
@@ -28,35 +28,46 @@ def gen_aug(text):
     prompt = (prompt_start + "\n\n---\n\n" + text)
 
     response = ""
-
-    # Set flag to true
-    flag = True
+    
+    # if prompt is too long, the API will not work. We need to make the prompt
+    # shorter recursively untill it is acceptable by the API
+    
+    # Cut 10% of the propmt each time.
     len_cut = int(len(prompt)*0.1)
-
+    
+    # Set the flag to true so that the loop starts running
+    flag = True
     while flag:
 
+        # Try this prompt
         try:
             result = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
                     messages=[{"role": "user", "content": prompt}]
                 )
+            
+            # If no error, do not further run the loop
             flag = False
 
+        # If the prompt is too long, make it shorter
         except:
             prompt = prompt[:-len_cut]
-
+    
+    # Extract the reponse
     response = result["choices"][0]["message"]["content"]
     
     return response
 
-# If we are continuing augmented data generation:
+# The pipeline can be stopped and continue working later on
+
+# If the file exists, we are continuing augmented data generation:
 if os.path.isfile(os.path.join(data_path,'tmp.csv')):
 
     # Load data
     df = pd.read_csv(os.path.join(data_path,'tmp.csv'))
     data_count = pd.read_csv(os.path.join(data_path,'data_count.csv'))
 
-# If we start generating data augmentation:
+# If the data does not exist, we start generating data augmentation:
 else:
 
     # Initialize data
@@ -68,6 +79,7 @@ else:
         data_count.loc[label, 'original'] = int(df['label'].value_counts()[label])
     data_count['aug'] = 0
 
+# Number of augmented data is the same as the majority class
 n_aug = int(np.max(data_count['original']))
 
 error_count = 0
@@ -78,7 +90,8 @@ while np.sum(data_count['aug']<n_aug*1.01)>0:
     for i in df.index:
 
         l = df.loc[i,'label']
-
+        
+        # If we have generated more than enough data for this class
         if data_count.loc[l, 'aug']>n_aug*1.01:
             continue
 
@@ -90,7 +103,8 @@ while np.sum(data_count['aug']<n_aug*1.01)>0:
         sleep(1)
 
         try:
-
+            
+            # Get the augmented text
             aug_texts = gen_aug(text)
 
             j =  df.loc[i,'num_aug_gen']
@@ -100,29 +114,36 @@ while np.sum(data_count['aug']<n_aug*1.01)>0:
             df.loc[i,'num_aug_gen'] = df.loc[i,'num_aug_gen'] + 1
 
             print(l, data_count.loc[l, 'aug'])
-
+        
+        # If there was an error, wait for 60 seconds
         except:
 
             sleep(60)
+            
+            # Count error
             error_count = error_count + 1
-    
+            
+            # If we have more than 5 errors, break
             if error_count>5:
                 raise Exception('Too many errors')
-
+        
+        # Save the data
         df.to_csv(os.path.join(data_path,'tmp.csv'), index=False)
         data_count.to_csv(os.path.join(data_path,'data_count.csv'), index=False)
 
-        
-aug_cols = []
 
+# Get the list of augmented columns
+aug_cols = []
 for col in df.columns:
     if col.startswith('tmp_aug'):
        	aug_cols.append(col)
 
+# get index of NaNs
 NaNs = df.isna()
 
 all_data = pd.DataFrame(columns=['org_ind', 'paragraph', 'label',])
 
+# Put different augmented samples from the same original sample in different rows
 counter = 0
 for i in df.index:
     
@@ -137,13 +158,19 @@ for i in df.index:
 
             counter += 1
 
+# Final output
 out = pd.DataFrame(columns=['org_ind', 'paragraph', 'label',])
 
 for l in set(all_data['label']):
     data_label = all_data.loc[all_data['label']==l, :]
     data_label_sampled = data_label.sample(n=n_aug, random_state=42)
     out = pd.concat([out, data_label_sampled])
-    
+
+# Rename the columns
 out.rename(columns={'paragraph': "text"}, inplace=True)
+
+# Shuffle
 out_shuff = out.sample(frac=1, random_state=42).reset_index(drop=True)
+
+# Save the data
 out_shuff.to_csv(os.path.join(data_path,'aug.csv'), index=False)
